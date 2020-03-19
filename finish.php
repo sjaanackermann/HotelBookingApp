141c147c8acfd69d69b37e828d62f8796831dbfe
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-03-19 07:41:08 +0200

    finish.php added

---
+++ finish.php
@@ -0,0 +1,27 @@
+<?php 
+//ADD EMAIL CODING TO THIS PAGE
+//require_once "incl/Email.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Completed Booking</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <div class="column">
+        <!-- Box that displays if a booking is made -->
+        <div class="box" id="move">
+            <h1 class="title"> Thank you for booking! </h1>
+            <div class="control" id="cancel">
+                <a href ="index.php"> <button class="button">Home</button></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
