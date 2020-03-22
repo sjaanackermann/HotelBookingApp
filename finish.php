3ab0370927ea039c6c68bceba67171b482118281
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-03-22 21:46:41 +0200

    finish.php updated

--- finish.php
+++ finish.php
@@ -40,23 +40,37 @@ use PHPMailer\PHPMailer\Exception;
         $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom($_SESSION['email'], 'Mailer');
+        $mail->setFrom($_SESSION['email'], 'Mailer');                      // Add a client email
         $mail->addAddress('scotlandbookings@scotland.com', 'Client');     // Add a recipient           
-        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addReplyTo('info@example.com', 'Information');              // Add reply to email
 
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
         $mail->Subject = 'Booking Alert';
         $mail->Body    = 
-                    "Dear Bookings Manager, <br> 
+                    "Dear Bookings Manager, <br><br>
                     Please see details of the booking new enquiry below: <br>
                     <b>Client Name & Surname:</b> ".$_SESSION['name']." ".$_SESSION['surname']."<br>
                     <b>Client Email:</b> ".$_SESSION['email']."<br>
+                    <b>Selected Hotel: </b>".
+                    (($_POST["hotel1"] == "hotel1") ? "Yotel Hotel" : "").
+                    (($_POST["hotel2"]) ? "Ibis Hotel" : "").
+                    (($_POST["hotel3"]) ? "Indigo Hotel" : "").
+                    (($_POST["hotel4"]) ? "Sandman Hotel" : "").
+                    (($_POST["hotel5"]) ? "Sleeperz Hotel" : "").
+                    (($_POST["hotel6"]) ? "Motel Hotel" : "").
+                    (($_POST["hotel7"]) ? "Radisson Hotel" : "").
+                    (($_POST["hotel8"]) ? "Residence Hotel" : "").
+                    (($_POST["hotel9"]) ? "Hampton Hotel" : "").
+                    (($_POST["hotel10"]) ? "Alexandra Hotel" : "").
+                    "<br>
                     <b>From :</b> ".$_SESSION['in']." to : ".$_SESSION['out']. "<br>";
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
         '<div class="emailFeedback">';
+        echo "<div class=\"column\">";
+        echo "<div class=\"box\" id=\"move\">";
         echo 'Thank you for your Enquiry, we will be in touch shortly to finalise the booking!';
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
