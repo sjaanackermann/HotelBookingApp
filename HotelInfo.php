ade7d501f57a12bb2459c5fad8aa1c0b7ebfea12
zacharyw16 <zacharyw230@gmail.com>
2020-03-22 19:10:52 +0200

    Add files via upload
    added to siplay.php

---
+++ HotelInfo.php
@@ -0,0 +1,22 @@
+<?php 
+//Required Includes
+require_once "incl/HotelClass.php";
+
+
+?>
+
+<?php
+
+//hotel ammenities///Name,Price,Wifi,pool,bar,spa,buffet, smoking, parking
+$Yotel = new Hotel("Yotel", 500, true, false, true, false, true, false, true);
+$Ibis = new Hotel("Ibis Styles", 750, true, true, true, false, true, false, true);
+$Indigo = new Hotel("Indigo", 1000, true, true, true, true, true, false, true);
+$Sandman = new Hotel("Sandman Signature", 800, true, false, true, false, false, true, false);
+$Sleeperz = new Hotel("Sleeperz", 600, false, false, true, false, false, true, false);
+$Motel = new Hotel("Motel One", 550, false, false, true, false, true, false, true);
+$Radisson = new Hotel("Radisson Red", 1000, true, true, true, true, true, false, true);
+$Residence = new Hotel("Residence Inn", 1000, true, false, true, true, true, false, true);
+$Hampton = new Hotel("Hampton", 1000, true, false, true, true, true, false, true);
+$Alexandra = new Hotel("Alexandra", 1200, true, false, true, true, true, false, true);
+$allHotels = array($Yotel, $Ibis, $Indigo, $Sandman, $Sleeperz, $Motel, $Radisson, $Residence, $Hampton, $Alexandra);
+?>
--- display.php
+++ display.php
@@ -14,14 +14,22 @@ require "incl/HotelInfo.php";
 <!DOCTYPE html>
 <html>
 <head>
+!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Confirm Booking</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-    <link rel="stylesheet" href="./css/styles.css">
+    <!-- My CSS -->
+	<link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
+<div class="display">
     <div class="column">
             <!-- PHP scripts that takes the information from the index page and displays it on this one -->
             <?php 
@@ -55,6 +63,7 @@ if ($_POST['hotel1']) {
         echo "</div>";
         echo "</div>";
         echo "</div>";
+		echo "</div>";
     //End of Div 
 }
 
